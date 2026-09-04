b796125ec5a486e79f8dbf4295935e00b940ac27
BadAcronym <terribleacronym@gmail.com>
2026-09-04 10:00:34 +0200

    add basic files

---
+++ include/datasurf_main.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+uint8_t* dsDeflate
+(
+    uint8_t *start
+);
---
+++ src/datasurf_algo_deflate.c
@@ -0,0 +1,13 @@
+#include "datasurf_main.h"
+
+#include <stdio.h>
+
+uint8_t* dsDeflate
+(
+    uint8_t *start
+){
+    //
+    fprintf(stderr, "\033[31;3mERROR: deflate not implemented yet.\033[0m\n");
+    return 0;
+    //
+}

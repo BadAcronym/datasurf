15aff181a087d0ab4831da1711a33e94a4e39de3
BadAcronym <terribleacronym@gmail.com>
2026-09-06 23:56:16 +0200

    ignore debug & warning in release, but not err

--- include/datasurf_info_macros.h
+++ include/datasurf_info_macros.h
@@ -8,19 +8,18 @@
 #define __LINE_STR __TO_STRING(__LINE__)
 #define __LOCATION__ "[" __FILE__ ":" __LINE_STR "]"
 
+#define DATASURF_ERROR(MESSAGE, ...) \
+        fprintf(stderr, "\033[31;1m" __LOCATION__ " ERROR: " \
+                MESSAGE "\033[0m\n", ##__VA_ARGS__)
+
+#define DATASURF_WARNING(MESSAGE, ...) \
+        fprintf(stdout, "\033[33;1m" __LOCATION__ " WARNING: " \
+                MESSAGE "\033[0m\n", ##__VA_ARGS__)
+
 #ifdef DEBUG
-    #define DATASURF_ERROR(MESSAGE, ...) \
-            fprintf(stderr, "\033[31;1m" __LOCATION__ " ERROR: " \
-                    MESSAGE "\033[0m\n", ##__VA_ARGS__)
-
-    #define DATASURF_WARNING(MESSAGE, ...) \
-            fprintf(stderr, "\033[33;1m" __LOCATION__ " WARNING: " \
-                    MESSAGE "\033[0m\n", ##__VA_ARGS__)
-
     #define DATASURF_DEBUG(MESSAGE, ...) \
-            fprintf(stderr, __LOCATION__": " MESSAGE "\n", ##__VA_ARGS__)
+            fprintf(stdout, __LOCATION__": " MESSAGE "\n", ##__VA_ARGS__)
 #else
-    #define DATASURF_ERROR(MESSAGE, ...)
     #define DATASURF_WARNING(MESSAGE, ...)
     #define DATASURF_DEBUG(MESSAGE, ...)
 #endif

7cfbacc2de1cede01f32961d138b085210c5aa74
BadAcronym <terribleacronym@gmail.com>
2026-09-06 23:44:42 +0200

    format, reorganize & repurpose macros

--- src/datasurf_algo_deflate.c
+++ src/datasurf_algo_deflate.c
@@ -1,7 +1,10 @@
 #include "datasurf_main.h"
 #include "datasurf_info_macros.h"
 
-#include <stdio.h>
+#define BTYPE_UNCROMPRESSED   0
+#define BTYPE_STATIC_HUFFMAN  1
+#define BTYPE_DYNAMIC_HUFFMAN 2
+#define BTYPE_RESERVED        3
 
 typedef struct DeflateBlock
 {
@@ -10,11 +13,6 @@ typedef struct DeflateBlock
     uint8_t BDATA  : 5;
 }
 DeflateBlock;
-
-#define BTYPE_UNCROMPRESSED   0
-#define BTYPE_STATIC_HUFFMAN  1
-#define BTYPE_DYNAMIC_HUFFMAN 2
-#define BTYPE_RESERVED        3
 
 uint64_t dsReadDeflate
 (
@@ -35,33 +33,34 @@ uint64_t dsReadDeflate
         block.BFINAL = src[i] & 1;
         if(block.BFINAL)
         {
-        	DATASURF_DEBUG(stdout, "BFINAL at byte %lu", i);
+            DATASURF_DEBUG("BFINAL at byte %lu", i);
             break;
         }
 
         block.BTYPE = (src[i] >> 1) & 3;
-        DATASURF_DEBUG(stdout, "BTYPE: %u", block.BTYPE);
+        DATASURF_DEBUG("BTYPE: %u", block.BTYPE);
+
         if(block.BTYPE == BTYPE_UNCROMPRESSED)
         {
             // 16 bits LEN
             // 16 bits NLEN
             // LEN bytes of actual, uncompressed data
-        	DATASURF_WARNING(stderr, "uncompressed block not implemented.");
+            DATASURF_ERROR("uncompressed block not implemented.");
             return false;
         }
         else if(block.BTYPE == BTYPE_STATIC_HUFFMAN)
         {
-        	DATASURF_WARNING(stderr, "static huffman block not implemented.");
+            DATASURF_ERROR("static huffman block not implemented.");
             return false;
         }
         else if(block.BTYPE == BTYPE_DYNAMIC_HUFFMAN)
         {
-        	DATASURF_WARNING(stderr, "dynamic huffman block not implemented.");
+            DATASURF_ERROR("dynamic huffman block not implemented.");
             return false;
         }
         else if(block.BTYPE == BTYPE_RESERVED)
         {
-        	DATASURF_WARNING(stderr, "BTYPE of 3 (bits 11) is reserved.");
+            DATASURF_ERROR("BTYPE of 3 (bits 11) is reserved.");
             return false;
         }
     }
--- src/datasurf_formats.c
+++ src/datasurf_formats.c
@@ -14,15 +14,15 @@ typedef struct ZLibInfo
 ZlibInfo;
 
 typedef union /*ZUnion*/{
-	uint8_t data[3];
+    uint8_t data[3];
 
-	ZlibInfo info;
+    ZlibInfo info;
 
-	struct{
-		uint8_t CMF;
-		uint8_t FLG;
-		uint8_t DICTID;
-	}orig;
+    struct{
+        uint8_t CMF;
+        uint8_t FLG;
+        uint8_t DICTID;
+    }orig;
 }ZUnion;
 
 bool dsReadZlibPtr
@@ -31,33 +31,33 @@ bool dsReadZlibPtr
     uint8_t  *dest,
     uint64_t maxDeflateLen
 ){
-	ZUnion uinfo = { .data = {zlib[0], zlib[1], zlib[2]} };
-	ZlibInfo info = uinfo.info;
-	uint8_t CMF    = zlib[0];//can be replaced by uinfo.orig.CMF
-	uint8_t FLG    = zlib[1];//can be replaced by uinfo.orig.FLG
-	uint8_t DICTID = zlib[2];//can be replaced by uinfo.orig.DICTID
+    ZUnion uinfo = { .data = {zlib[0], zlib[1], zlib[2]} };
+    ZlibInfo info = uinfo.info;
+    uint8_t CMF    = zlib[0];//can be replaced by uinfo.orig.CMF
+    uint8_t FLG    = zlib[1];//can be replaced by uinfo.orig.FLG
+    uint8_t DICTID = zlib[2];//can be replaced by uinfo.orig.DICTID
 
     if(info.CM != 8)
     {
-    	DATASURF_ERROR(stderr, "could not validate CMF in zlib data. "
+        DATASURF_ERROR("could not validate CMF in zlib data. "
                 "expected: 8, got: %u.", info.CM);
         return false;
     }
 
     if((CMF * 256 + FLG) % 31 != 0)
     {
-    	DATASURF_ERROR(stderr, "Failed zlib header integrity check: CMF*256 + FLG "
+        DATASURF_ERROR("Failed zlib header integrity check: CMF*256 + FLG "
                 "is not a multiple of 31, but instead: %u.",
                 CMF * 256 + FLG);
         return false;
     }
 
-    DATASURF_DEBUG(stdout, "CM:     %u", info.CM);
-    DATASURF_DEBUG(stdout, "CINFO:  %u", info.CINFO);
-    DATASURF_DEBUG(stdout, "FCHECK: %u", info.FCHECK);
-    DATASURF_DEBUG(stdout, "FDICT:  %u", info.FDICT);
-    DATASURF_DEBUG(stdout, "FLEVEL: %u", info.FLEVEL);
-    DATASURF_DEBUG(stdout, "DICTID: %u", DICTID);
+    DATASURF_DEBUG("CM:     %u", info.CM);
+    DATASURF_DEBUG("CINFO:  %u", info.CINFO);
+    DATASURF_DEBUG("FCHECK: %u", info.FCHECK);
+    DATASURF_DEBUG("FDICT:  %u", info.FDICT);
+    DATASURF_DEBUG("FLEVEL: %u", info.FLEVEL);
+    DATASURF_DEBUG("DICTID: %u", DICTID);
 
     uint32_t madeChecksum = 0;
 
@@ -66,8 +66,8 @@ bool dsReadZlibPtr
 
     uint32_t readChecksum = *(uint32_t*)(&zlib[3 + bytesRead]);
 
-    DATASURF_DEBUG(stdout, "made checksum: %u", madeChecksum);
-    DATASURF_DEBUG(stdout, "read checksum: %u", readChecksum);
+    DATASURF_DEBUG("made checksum: %u", madeChecksum);
+    DATASURF_DEBUG("read checksum: %u", readChecksum);
 
     return madeChecksum == readChecksum;
 }

713359eb06c1b803180433c95707c5cc75b1e021
badacronym <terribleacronym@gmail.com>
2026-09-07 15:21:40 +0200

    clean correctly

---
+++ clean.ps1
@@ -0,0 +1,32 @@
+Write-Host "cleaning up datasurf builds..." -Fore Yellow
+
+if(Test-Path "./bin")
+{
+    rm "./bin/" -Recurse -Force
+}
+
+foreach($file in (gci *.o))
+{
+    if(Test-Path $file)
+    {
+        Remove-Item $file
+    }
+}
+
+foreach($file in (gci *.exe))
+{
+    if(Test-Path $file)
+    {
+        Remove-Item $file
+    }
+}
+
+foreach($file in (gci *.pdb))
+{
+    if(Test-Path $file)
+    {
+        Remove-Item $file
+    }
+}
+
+Write-Host "cleaned puddle!`n" -Fore Green
